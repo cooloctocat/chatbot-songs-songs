27c58efb60b11632a0aeedb08c79295752732a32
Betty Chow <bettychchow@gmail.com>
2017-12-31 22:38:43 -0800

    Edit config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAC3tV8sOkcBAEuzhEzHtwV8gyXwxSGZANuHu8nJeZBQUdfKTWZC7H58SFa4O3woqppTFxLJqZCCqFZCPNl9kXzCrw8IZA4tgesFXXRZBMdsZBowoJGZAyl2e2PFV6dX1yY8C73gyZAZB0bTZBp4yZAOPy82HDxztZBkbMSZAj1M6zisnsX6wZDZD',
     FB_VERIFY_TOKEN: '',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://songsandsongs.herokuapp.com/",
 };
